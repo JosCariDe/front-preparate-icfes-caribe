16ab92c897a41127211baf3a051fa3b4af0c672f
joseLaptop <josenarvaezcariaga@gmail.com>
2025-05-31 20:49:57 -0500

    HomeScreen/feat/ Barra de busqueda agregado

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -11,45 +11,73 @@ class HomeScreen extends StatelessWidget {
         child: Column(
           children: [
             //! INFORMACION DEL USER
+            InforUser(),
+            //! BARRA DE BUSQUEDA
             Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-              child: Row(
-                children: [
-                  CircleAvatar(
-                    radius: 30,
-                    backgroundColor: primaryColor,
-                    child: Icon(
-                      Icons.person,
-                      size: 40,
-                      color: secondaryBackground,
-                    ),
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: 'Encuentra tu actividad!!',
+                  prefixIcon: Icon(Icons.search, color: Colors.grey),
+                  filled: true,
+                  fillColor: Colors.white,  //? COLOR DE RELLENO
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    borderSide: BorderSide.none //? SIN BORDE VISIBLE
                   ),
-                  const SizedBox(width: 15,),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Jose Narvaez',
-                        style: TextStyle(
-                          color: primaryText
-                        ),
-                      ),
-                      Text(
-                        'Listo para el 400 REY',
-                        style: Theme.of(context).textTheme.bodyMedium,
-                      )
-                    ],
-                  )
-                ],
+                  contentPadding: 
+                    EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+                ),
               ),
-              
-              ),
-            //! BARRA DE BIUSQUEDA
+              )
             //TODO SECCION CLASES ICFES
             //TODO SECCION AVANCE
           ],
         ),
       ),
     );
+  }
+}
+
+class InforUser extends StatelessWidget {
+  const InforUser({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      child: Row(
+        children: [
+          CircleAvatar(
+            radius: 30,
+            backgroundColor: primaryColor,
+            child: Icon(
+              Icons.person,
+              size: 40,
+              color: secondaryBackground,
+            ),
+          ),
+          const SizedBox(width: 15,),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Jose Narvaez',
+                style: TextStyle(
+                  color: primaryText
+                ),
+              ),
+              Text(
+                'Listo para el 400 REY',
+                style: Theme.of(context).textTheme.bodyMedium,
+              )
+            ],
+          )
+        ],
+      ),
+      
+      );
   }
 }
